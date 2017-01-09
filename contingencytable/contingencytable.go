dce7180d103afafa1739bc6d20589c2440b9271e
Joao Brito <joaojr.89@gmail.com>
2017-01-09 16:01:52 -0200

    add stride map

--- contingencytable/contingencytable.go
+++ contingencytable/contingencytable.go
@@ -10,6 +10,11 @@ type Sparse struct {
 // LoadFromData creates a new contingey table from data
 func LoadFromData(data [][]int, cardinality []int) *Sparse {
 	sp := Sparse{make(map[int]int), make(map[int]int)}
-
+	stride := 1
+	for k, v := range cardinality {
+		sp.strideMap[k] = stride
+		stride *= v
+	}
+	// TODO count data
 	return &sp
 }
