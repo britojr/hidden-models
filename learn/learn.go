a70a619596c2ac93ec982133473ddc357eb44b6a
Joao Brito <joaojr.89@gmail.com>
2017-02-04 15:36:29 -0200

    implement learner methods

--- learn/learn.go
+++ learn/learn.go
@@ -1,1 +1,61 @@
 package learn
+
+import (
+	"github.com/britojr/playgo/counting/bitcounter"
+	"github.com/britojr/playgo/filehandler"
+	"github.com/britojr/playgo/junctree"
+	"github.com/britojr/playgo/utils"
+	"github.com/britojr/tcc/generator"
+)
+
+// Learner ..
+type Learner struct {
+	//parameters
+	iterations int
+	treewidth  int
+	n          int
+	dataset    *filehandler.DataSet
+	counter    *bitcounter.BitCounter
+}
+
+// New ..
+func New() *Learner {
+	l := new(Learner)
+	l.iterations = 100
+	l.treewidth = 3
+	return l
+}
+
+// LoadDataSet ..
+func (l *Learner) LoadDataSet(dsfile string, delimiter rune, dsHdrlns filehandler.HeaderFlags) {
+	l.dataset = filehandler.NewDataSet(dsfile, delimiter, dsHdrlns)
+	l.dataset.Read()
+	l.counter = bitcounter.NewBitCounter()
+	l.counter.LoadFromData(l.dataset.Data(), l.dataset.Cardinality())
+	l.n = len(l.dataset.Cardinality())
+}
+
+// BestJuncTree ..
+func (l *Learner) BestJuncTree() *junctree.JuncTree {
+	bestStruct, bestScore := l.newRandomStruct()
+	for i := 1; i < l.iterations; i++ {
+		currStruct, currScore := l.newRandomStruct()
+		if currScore > bestScore {
+			bestScore = currScore
+			bestStruct = currStruct
+		}
+	}
+	return bestStruct
+}
+
+func calcLL(jt *junctree.JuncTree) float64 {
+	return 0.0
+}
+
+func (l *Learner) newRandomStruct() (*junctree.JuncTree, float64) {
+	T, iphi, err := generator.RandomCharTree(l.n, l.treewidth)
+	utils.ErrCheck(err, "")
+	jt := junctree.FromCharTree(T, iphi)
+	score := calcLL(jt)
+	return jt, score
+}
