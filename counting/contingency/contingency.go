8cdf0953bd42ae934012202226970366a51a0d81
Joao Brito <joaojr.89@gmail.com>
2017-02-06 00:26:46 -0200

    add sumout to cont table

--- counting/contingency/contingency.go
+++ counting/contingency/contingency.go
@@ -26,9 +26,9 @@ func (t *Table) SetCardinality(cardin *[]int) {
 	t.cardinality = cardin
 }
 
-// SetValues ..
-func (t *Table) SetValues(values []int) {
-	t.occurrences = values
+// SetOccurrences ..
+func (t *Table) SetOccurrences(occurrences []int) {
+	t.occurrences = occurrences
 }
 
 // Size ..
@@ -44,4 +44,30 @@ func (t *Table) Get(i int) int {
 // GetOccurrences ..
 func (t *Table) GetOccurrences() []int {
 	return t.occurrences
+}
+
+// SumOut ..
+func (t *Table) SumOut(x int) (r *Table) {
+	// TODO: fix and test this sumout
+	stride := 1
+	index := 0
+	for i, v := range t.variables {
+		if v == x {
+			index = i
+			break
+		}
+		stride *= (*t.cardinality)[v]
+	}
+	values := []int(nil)
+	base := 0
+	v := 0
+	for j := 0; j < (*t.cardinality)[x]; j++ {
+		v += t.occurrences[base+(stride*j)]
+	}
+	values = append(values, v)
+
+	auxvar := append([]int(nil), t.variables[:index]...)
+	auxvar = append(auxvar, t.variables[index+1:]...)
+	r = NewTable(auxvar, values, t.cardinality)
+	return
 }
