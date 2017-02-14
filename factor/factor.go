143906ddf0c90c13ea44a119b4920f203db6b459
Joao Brito <joaojr.89@gmail.com>
2017-02-14 16:46:25 -0200

    add factor method signatures

--- factor/factor.go
+++ factor/factor.go
@@ -1,1 +1,34 @@
 package factor
+
+import "github.com/britojr/kbn/assignment"
+
+// Factor ..
+type Factor struct {
+	cardin  []int
+	varlist []int
+	stride  map[int]int
+	values  float64
+}
+
+// New ..
+func New(varlist []int, cardin []int, values []float64) *Factor {
+	f := new(Factor)
+	return f
+}
+
+// Get ..
+func (f *Factor) Get(assig assignment.Assignment) float64 {
+	return 0.0
+}
+
+// Product ..
+func (f *Factor) Product(g *Factor) *Factor {
+	h := new(Factor)
+	return h
+}
+
+// SumOut ..
+func (f *Factor) SumOut(x int) *Factor {
+	h := new(Factor)
+	return h
+}
