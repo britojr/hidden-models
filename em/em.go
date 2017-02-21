ea9954e0afb468ae512ccb7c50b7806ce0af00a3
Joao Brito <joaojr.89@gmail.com>
2017-02-21 12:56:47 -0300

    create pack em

---
+++ em/em.go
@@ -0,0 +1,14 @@
+// Package em implements expectation-maximization algorithm
+package em
+
+import (
+	"github.com/britojr/kbn/cliquetree"
+	"github.com/britojr/kbn/filehandler"
+)
+
+var maxiterations = 100
+
+// ExpectationMaximization ..
+func ExpectationMaximization(ct cliquetree.CliqueTree, ds filehandler.DataSet) {
+
+}
