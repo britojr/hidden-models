05689ad2cdc24fbfc85d559313bead7cbfb73d70
Joao Brito <joaojr.89@gmail.com>
2017-05-11 18:55:35 -0300

    add uai to fg libdai converter

---
+++ cmd/uaitofg/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/britojr/kbn/mrf"
+	"github.com/britojr/kbn/utils"
+)
+
+var (
+	mkfile string
+	fgfile string
+)
+
+func parseArgs() {
+	if len(os.Args) < 3 {
+		fmt.Println("Please enter both file names.")
+		return
+	}
+	mkfile = os.Args[1]
+	fgfile = os.Args[2]
+}
+
+func main() {
+	parseArgs()
+	// read MRF
+	f, err := os.Open(mkfile)
+	utils.ErrCheck(err, fmt.Sprintf("Can't create file %v", mkfile))
+	mk := mrf.LoadFromUAI(f)
+	f.Close()
+	if mk == nil {
+		fmt.Printf("an error occurred while loading file %v\n", mkfile)
+		return
+	}
+
+	// save FG
+	f, err = os.Create(fgfile)
+	utils.ErrCheck(err, fmt.Sprintf("Can't create file %v", fgfile))
+	mk.SaveOnLibdaiFormat(f)
+	f.Close()
+}
