a68e0417f43833aaa5ed4cab45571bd0294029f4
Joao Brito <joaojr.89@gmail.com>
2017-06-04 13:42:52 -0300

    add marginals load and compare

--- learn/structure.go
+++ learn/structure.go
@@ -9,6 +9,7 @@ import (
 	"github.com/britojr/kbn/dataset"
 	"github.com/britojr/kbn/likelihood"
 	"github.com/britojr/kbn/utl"
+	"github.com/britojr/kbn/utl/stats"
 )
 
 // SampleStructure samples a cliquetree structure with limited treewidth
@@ -22,6 +23,22 @@ func SampleStructure(ds *dataset.Dataset, k, h int, ctfile string) (float64, tim
 		saveCliqueTree(ct, ctfile)
 	}
 	return sll, elapsed
+}
+
+// SaveMarginas load a cliquetree to calculate and save its marginals
+func SaveMarginas(ctfile, marfile string) {
+	ct := loadCliqueTree(ctfile)
+	saveCTMarginals(ct, -1, marfile)
+}
+
+// MarginalsMSE compares two marginals and return the Mean Squared Error
+func MarginalsMSE(exact, approx string) (mse float64) {
+	e, a := loadMarginals(exact), loadMarginals(approx)
+
+	for i := range e {
+		mse += stats.MSE(e[i], a[i])
+	}
+	return mse / float64(len(e))
 }
 
 func loadCliqueTree(fname string) *cliquetree.CliqueTree {
@@ -40,7 +57,11 @@ func saveCTMarginals(ct *cliquetree.CliqueTree, obs int, fname string) {
 	f := utl.CreateFile(fname)
 	defer f.Close()
 	ma := ct.Marginals()
-	writeMarginals(f, ma[:obs])
+	if obs > 0 {
+		writeMarginals(f, ma[:obs])
+	} else {
+		writeMarginals(f, ma)
+	}
 }
 
 func writeMarginals(w io.Writer, ma [][]float64) {
@@ -52,4 +73,25 @@ func writeMarginals(w io.Writer, ma [][]float64) {
 			fmt.Fprintf(w, "%.5f ", v)
 		}
 	}
+}
+
+func loadMarginals(fname string) [][]float64 {
+	f := utl.OpenFile(fname)
+	defer f.Close()
+	return readMarginals(f)
+}
+
+func readMarginals(r io.Reader) (ma [][]float64) {
+	fmt.Fscanln(r)
+	var n int
+	fmt.Fscanf(r, "%d", &n)
+	ma = make([][]float64, n)
+	for i := range ma {
+		fmt.Fscanf(r, "%d", &n)
+		ma[i] = make([]float64, n)
+		for j := range ma[i] {
+			fmt.Fscanf(r, "%f", &ma[i][j])
+		}
+	}
+	return
 }
