f2ff5fea1ab1383ae8e268f29e10340a95a13c17
Joao Brito <joaojr.89@gmail.com>
2017-06-06 16:10:18 -0300

    add metrics

--- cmd/margerr/margerr.go
+++ cmd/margerr/margerr.go
@@ -3,15 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/britojr/kbn/learn"
+	"github.com/britojr/kbn/utl"
 	"github.com/britojr/kbn/utl/stats"
+	"github.com/gonum/floats"
+)
+
+const (
+	metrMSE = iota
+	metrCrossEntropy
+	metrInverse
+	metrL1
+	metrL2
 )
 
 var (
-	exact, compfunc string
+	exact       string
+	metrChoices map[string]int
 )
 
 func init() {
@@ -29,20 +42,32 @@ func parseFlags() {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+
+	metrChoices = map[string]int{
+		"mse":     metrMSE,
+		"entropy": metrCrossEntropy,
+		"inverse": metrInverse,
+		"l1":      metrL1,
+		"l2":      metrL2,
+	}
 }
 
 func main() {
 	marfs, _ := filepath.Glob("*.mar")
 	fmt.Println(marfs)
-	fmt.Printf("marfile,mse,cross_entropy,variance\n")
+
+	mp := utl.CreateFile(fmt.Sprintf("marginals_%v.txt", time.Now().Format(time.RFC3339)))
+	defer mp.Close()
+	cfuncs := []string{"mse", "entropy", "inverse", "l1", "l2"}
+	fmt.Fprintln(mp, utl.Sprintc("marfile", cfuncs))
+
 	for _, marf := range marfs {
 		if marf != exact {
-			fmt.Printf("%v,%v,%v,%v",
-				marf,
-				compare(exact, marf, 0),
-				compare(exact, marf, 1),
-				compare(exact, marf, 1),
-			)
+			d := []float64(nil)
+			for _, v := range cfuncs {
+				d = append(d, compare(exact, marf, metrChoices[v]))
+			}
+			fmt.Fprintln(mp, utl.Sprintc(marf, d))
 		}
 	}
 }
@@ -50,12 +75,16 @@ func main() {
 func compare(exact, approx string, compfunc int) (d float64) {
 	e, a := learn.LoadMarginals(exact), learn.LoadMarginals(approx)
 	switch compfunc {
-	case 0:
+	case metrMSE:
 		d = margMSE(e, a)
-	case 1:
+	case metrCrossEntropy:
 		d = margCrossEntropy(e, a)
-	case 2:
-		d = margVariance(e, a)
+	case metrInverse:
+		d = margCrossEntropy(a, e)
+	case metrL1:
+		d = margDistance(a, e, 1)
+	case metrL2:
+		d = margDistance(a, e, 2)
 	}
 	return
 }
@@ -69,7 +98,14 @@ func margMSE(e, a [][]float64) (mse float64) {
 
 func margCrossEntropy(e, a [][]float64) (c float64) {
 	for i := range e {
-		c += stats.CrossEntropy(e[i], a[i])
+		c += crossEntropy(e[i], a[i])
+	}
+	return c / float64(len(e))
+}
+
+func margDistance(e, a [][]float64, l float64) (c float64) {
+	for i := range e {
+		c += floats.Distance(e[i], a[i], l)
 	}
 	return c / float64(len(e))
 }
@@ -83,4 +119,14 @@ func margVariance(a, b [][]float64) float64 {
 		}
 	}
 	return d / float64(c)
+}
+
+func crossEntropy(xs, ys []float64) (c float64) {
+	for i, v := range xs {
+		// c -= v * math.Log(ys[i])
+		if ys[i] != 0 {
+			c -= v * math.Log(ys[i])
+		}
+	}
+	return
 }
